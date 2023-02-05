647bd26a187cc6797868c0727416e49d6f3de7e8
mbk1991 <mbk1991@naver.com>
2023-02-05 19:33:54 +0900

    230205 19:33 chap01 list_interface :java list캡슐화 및 jUnit 타입 체크

--- Chap01_Interface/src/ex01_ComparableInterface/Comparable.java
+++
@@ -1,5 +0,0 @@
-package ex01_ComparableInterface;
-
-public interface Comparable<T> {
-	public int compareTo(T o);
-}
---
+++ Chap01_Interface/src/test/java/ex01_list_interface/ListClientExampleTest.java
@@ -0,0 +1,22 @@
+package ex01_list_interface;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class ListClientExampleTest {
+	//Unit테스트를 이용하여 ListClientExample의 getList로 생성한 list객체가 LinkedList.class 타입인지 확인하고 있다.
+	//
+	@Test
+	public void testListClientExample() {
+		ListClientExample lce = new ListClientExample();
+		@SuppressWarnings("rawtypes")
+		List list = lce.getList();
+		assertThat(list, instanceOf(LinkedList.class));
+	}
+
+}
